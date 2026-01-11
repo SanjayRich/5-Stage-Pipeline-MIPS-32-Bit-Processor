ce9c3eb032fdd4b5f233c59bd92abf55235ce298
Sanjay Karthikeyan <sanjayelavarasank590@gmail.com>
2026-01-12 00:17:09 +0530

    Add files via upload

---
+++ Test1.asm
@@ -0,0 +1,10 @@
+# MIPS32 Assembly Program
+
+ADDI R1, R0, 10     # R1 = 10
+ADDI R2, R0, 20     # R2 = 20
+ADDI R3, R0, 25     # R3 = 25
+
+ADD  R4, R1, R2     # R4 = R1 + R2 = 30
+ADD  R5, R4, R3     # R5 = R4 + R3 = 55
+
+HLT                 # Halt execution
